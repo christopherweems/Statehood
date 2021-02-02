7513af6d0d6a565825251831175ab21012ca3b17
Christopher Weems <hello@christopherweems>
2021-02-02 16:03:57 -0500

    Update Swift tools to 5.4

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.4
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -16,8 +16,8 @@ let package = Package(
             targets: ["Statehood"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-algorithms.git", .upToNextMajor(from: "0.0.1")),
-        .package(url: "https://github.com/christopherweems/Resultto.git", .upToNextMajor(from: "0.0.2")),
+        .package(url: "https://github.com/apple/swift-algorithms.git", .upToNextMajor(from: "0.0.2")),
+        .package(url: "https://github.com/christopherweems/Resultto.git", .upToNextMajor(from: "0.1.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
