d7fcd80585fb0af5f3e0e756b84312b17b631653
Christopher Weems <hello@christopherweems.com>
2021-09-29 12:15:49 -0400

    Update `swift-algorithms` dependency version to 1.0.0

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
             targets: ["Statehood"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-algorithms.git", .upToNextMajor(from: "0.0.2")),
-        .package(url: "https://github.com/christopherweems/Resultto.git", .upToNextMajor(from: "0.1.0")),
+        .package(url: "https://github.com/apple/swift-algorithms.git", from: "1.0.0"),
+        .package(url: "https://github.com/christopherweems/Resultto.git", from: "0.1.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
