52540e6d4a634bf0bf654d57018d6575862b6044
Christopher Weems <hello@christopherweems.com>
2021-12-20 09:15:41 -0500

    Remove test boilerplate not required since Swift 5.4

--- Tests/StatehoodTests/StatehoodTests.swift
+++ Tests/StatehoodTests/StatehoodTests.swift
@@ -2,18 +2,7 @@ import XCTest
 @testable import Statehood
 
 final class StatehoodTests: XCTestCase {
-    static var allTests = [
-        ("testAllUSPSAbbreviationsUnique", testAllUSPSAbbreviationsUnique),
-    ]
-}
-
-extension StatehoodTests {
     func testAllUSPSAbbreviationsUnique() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertFalse(!true)
-        
         var seen = Set<String>()
         
         State.allCases.forEach {
