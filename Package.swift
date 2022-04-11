594eb09beede2e31463e2d4bd75985f46ee58569
Christopher Weems <hello@christopherweems.com>
2022-04-11 17:59:33 -0400

    Update minimum `Resultto` dependency version to `0.2.2`

--- Package.swift
+++ Package.swift
@@ -1,12 +1,10 @@
 // swift-tools-version:5.6
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Statehood",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Statehood",
             targets: ["Statehood"]),
@@ -14,18 +12,18 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-algorithms.git", from: "1.0.0"),
-        .package(url: "https://github.com/christopherweems/Resultto.git", from: "0.2.1"),
+        .package(url: "https://github.com/christopherweems/Resultto.git", from: "0.2.2"),
         
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Statehood",
             dependencies: [
                 .product(name: "Algorithms", package: "swift-algorithms"),
                 .product(name: "Resultto", package: "Resultto"),
             ]),
+        
+        /* Tests */
         
         .testTarget(
             name: "StatehoodTests",
