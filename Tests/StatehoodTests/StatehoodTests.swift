113921bb8b47c4206b9dca4f1c5b856778388f22
Christopher Weems <hello@christopherweems.com>
2022-04-21 13:39:22 -0400

    Add `StatehoodTests.testNameFormatting()`

--- Tests/StatehoodTests/StatehoodTests.swift
+++ Tests/StatehoodTests/StatehoodTests.swift
@@ -2,6 +2,67 @@ import XCTest
 @testable import Statehood
 
 final class StatehoodTests: XCTestCase {
+    func testNameFormatting() {
+        let stateNames: [(String, State)] = [
+            ("Deleware", .deleware),
+            ("Pennsylvania", .pennsylvania),
+            ("New Jersey", .newJersey),
+            ("Georgia", .georgia),
+            ("Connecticut", .connecticut),
+            ("Massachusetts", .massachusetts),
+            ("Maryland", .maryland),
+            ("South Carolina", .southCarolina),
+            ("New Hampshire", .newHampshire),
+            ("Virginia", .virginia),
+            ("New York", .newYork),
+            ("North Carolina", .northCarolina),
+            ("Rhode Island", .rhodeIsland),
+            ("Vermont", .vermont),
+            ("Kentucky", .kentucky),
+            ("Tennessee", .tennessee),
+            ("Ohio", .ohio),
+            ("Louisiana", .louisiana),
+            ("Indiana", .indiana),
+            ("Mississippi", .mississippi),
+            ("Illinois", .illinois),
+            ("Alabama", .alabama),
+            ("Maine", .maine),
+            ("Missouri", .missouri),
+            ("Arkansas", .arkansas),
+            ("Michigan", .michigan),
+            ("Florida", .florida),
+            ("Texas", .texas),
+            ("Iowa", .iowa),
+            ("Wisconsin", .wisconsin),
+            ("California", .california),
+            ("Minnesota", .minnesota),
+            ("Oregon", .oregon),
+            ("Kansas", .kansas),
+            ("West Virginia", .westVirginia),
+            ("Nevada", .nevada),
+            ("Nebraska", .nebraska),
+            ("Colorado", .colorado),
+            ("North Dakota", .northDakota),
+            ("South Dakota", .southDakota),
+            ("Montana", .montana),
+            ("Washington", .washington),
+            ("Idaho", .idaho),
+            ("Wyoming", .wyoming),
+            ("Utah", .utah),
+            ("Oklahoma", .oklahoma),
+            ("New Mexico", .newMexico),
+            ("Arizona", .arizona),
+            ("Alaska", .alaska),
+            ("Hawaii", .hawaii),
+        ]
+        
+        for (stateName, state) in stateNames {
+            XCTAssertEqual(stateName, state.fullName)
+            
+        }
+        
+    }
+    
     func testAllUSPSAbbreviationsUnique() {
         var seen = Set<String>()
         
